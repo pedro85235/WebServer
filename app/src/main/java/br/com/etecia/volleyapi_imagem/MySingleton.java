c93dfd1a5f287c40252e5fb6e806d759eff46235
pedro85235 <pedroalvesmota85235@gmail.com>
2021-09-22 22:17:47 -0300

    Clone

---
+++ app/src/main/java/br/com/etecia/volleyapi_imagem/MySingleton.java
@@ -0,0 +1,39 @@
+package br.com.etecia.volleyapi_imagem;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MySingleton {
+
+    //variaveis globais para conexão e para os protocolos de comunicação
+    private static MySingleton myInstance;
+    private RequestQueue requestQueue;
+    private static Context myContext;
+
+
+    public MySingleton(Context context) {
+        myContext = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(myContext.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public static synchronized MySingleton getInstance(Context context) {
+        if (myInstance == null) {
+            myInstance = new MySingleton(context);
+        }
+        return myInstance;
+    }
+
+    public <T> void addToRequestQue(Request<T> request) {
+        requestQueue.add(request);
+    }
+}
